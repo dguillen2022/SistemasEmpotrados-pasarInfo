6a5a44e90667e376034c1b7d575daec5eb0a1765
Daniel Guillén <d.guillen.2022@alumnos.urjc.es>
2024-10-17 19:26:54 +0200

    Solo queda ejecutar y hacer gráfico y tabla

--- Practica2/src/cyclictestURJC.c
+++ Practica2/src/cyclictestURJC.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sched.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,29 +12,31 @@
 #include <string.h>
 
 #define PRIORITY 99
-#define _GNU_SOURCE
-#define SLEEP_TIME 0.5
+#define SLEEP_TIME 0.5e9
+#define DURATION_NS 60e9
 
 struct data {
-    int cpu
-    double *latency
+    int cpu;
+    double max_latency;
+    double avg_latency;
+    double *latencies;
+    int iteration;
 };
 
 void *thread_function(void *thread_info_ptr) {
-
-    struct timespec init_time, final_time;
     struct data *thread_info = (struct data *)thread_info_ptr;
-    double init_seconds, final_seconds, cost;
-    int id_clock=CLOCK_MONOTONIC
+    struct timespec init_time, final_time, init_thread_time;
+
+    long init_ns, final_ns=0, init_thread_ns, latency, total_latency=0; 
+    long max_latency=0, avg_latency;
+    int iterations=0, max_iterations;
+    int id_clock=CLOCK_MONOTONIC;
 
     int policy=SCHED_FIFO;
     struct sched_param param;
     param.sched_priority = PRIORITY;
 
-    //latencies = 
-
-    int cpu_id=thread_info.cpu;
-
+    int cpu_id=thread_info->cpu;
     cpu_set_t cpuset;
 
     CPU_ZERO(&cpuset);
@@ -48,23 +51,48 @@ void *thread_function(void *thread_info_ptr) {
     if (s2 != 0)
         errx(EXIT_FAILURE, "pthread_setaffinity_np failed: %d", s2);
 
-    if (clock_gettime(id_clock, &init_time) != 0) {
+    max_iterations = DURATION_NS/SLEEP_TIME;
+    thread_info->latencies=(double *) malloc(sizeof(double)*max_iterations);
+
+    if (clock_gettime(id_clock, &init_thread_time) != 0) {
         perror("Error al obtener tiempo final");
         exit(-1);
     }
-    init_seconds=init_time.tv_sec+init_time.tv_nsec/1e9;
+    init_thread_ns=init_thread_time.tv_sec*1e9+init_thread_time.tv_nsec;
 
-    usleep(SLEEP_TIME * 1e6);
-
-    if (clock_gettime(id_clock, &final_time) != 0) {
+    while (final_ns - init_thread_ns < DURATION_NS) {
+        if (clock_gettime(id_clock, &init_time) != 0) {
             perror("Error al obtener tiempo final");
             exit(-1);
+        }
+        init_ns = init_time.tv_sec*1e9 + init_time.tv_nsec;
+
+        usleep(SLEEP_TIME/1e3);
+
+        if (clock_gettime(id_clock, &final_time) != 0) {
+                perror("Error al obtener tiempo final");
+                exit(-1);
+        }
+        final_ns=final_time.tv_sec*1e9+final_time.tv_nsec;
+
+        latency=final_ns-init_ns-(SLEEP_TIME);
+        total_latency+=latency;
+        thread_info->latencies[iterations]=latency;
+
+        if (latency>max_latency){
+            max_latency=latency;
+        }
+
+        iterations++;
+        if (iterations >= max_iterations) {
+            break;
+        }
     }
-    final_seconds=final_time.tv_sec+final_time.tv_nsec/1e9;
+    thread_info->iteration=iterations;
+    thread_info->avg_latency=total_latency/iterations;
+    thread_info->max_latency=max_latency;
 
-    thread_info->latency=final_seconds - init_seconds - SLEEP_TIME;
-
-   
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -77,6 +105,10 @@ int main(int argc, char *argv[]) {
     pthread_t array_threads[num_cores];
     int num_thread_array[num_cores];
 
+    long double total_avg_latency=0, total_max_latency=0, avg_total_avg_latency;
+    int avg_latency_ns, max_latency_ns, iterations;
+
+
     struct data *thread_info[num_cores];
 
     for (int i = 0; i < num_cores; i++){
@@ -86,11 +118,48 @@ int main(int argc, char *argv[]) {
              (void *)thread_info[i]);
     }
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < num_cores; i++){
         pthread_join(array_threads[i], NULL);
+
+        avg_latency_ns=(int)(thread_info[i]->avg_latency);
+        max_latency_ns=(int)(thread_info[i]->max_latency);
+        iterations = thread_info[i]->iteration;
+
+        printf("[%i]    latencia media = %i ns. | max = %i ns\n", 
+             i, avg_latency_ns, max_latency_ns);
+
+        total_avg_latency+=avg_latency_ns;
+
+        if (max_latency_ns>total_max_latency) {
+            total_max_latency=max_latency_ns;
+        }
     }
-    
+
+    avg_total_avg_latency=total_avg_latency/num_cores;
+    printf("\nTotal latencia media = %i ns. | max = %i ns\n",
+     (int)(avg_total_avg_latency), (int)(total_max_latency));
+
+
+    FILE *csv_file = fopen("cyclictestURJC.csv", "w");
+    if (csv_file == NULL) {
+        perror("Error al abrir el archivo CSV");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(csv_file, "CPU,NUMERO_ITERACION,LATENCIA\n");
+
+    for (int i=0;i<num_cores;i++) {
+        for(int j=0;j<iterations;j++) {
+            fprintf(csv_file, "%i,%i,%i\n", i, j, (int)(thread_info[i]->latencies[j]));
+        }
+    }
+    fclose(csv_file);
+
+    for (int i = 0; i < num_cores; i++) {
+        free(thread_info[i]->latencies);
+        free(thread_info[i]);
+    }
+
     pthread_exit(NULL);
-    
+
     return 0;
 }
